9e0b04aeac698bb2af3e86ee28f9cbbe88b2cd74
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-06-02 00:47:39 +0300

    add weather

--- frontend/src/pages/Events/EventDetail/EventDetail.jsx
+++ frontend/src/pages/Events/EventDetail/EventDetail.jsx
@@ -4,7 +4,9 @@ import { weekdays } from "../../../data/data";
 import useAxios from "../../../hooks/useAxios";
 import "./EventDetail.css";
 import MapView from "./MapView";
-
+import Weather from "./Weather";
+import { IoMdTime } from "react-icons/io";
+import { IoLocationOutline } from "react-icons/io5";
 const EventDetail = () => {
   const { id } = useParams();
   const [event, setEvent] = useState({});
@@ -26,12 +28,21 @@ const EventDetail = () => {
       <div className="sticky">
         <div className="eventInfo">
           <h3>{event.title}</h3>
-          <p>
-            {weekdays[dayIndex]}, {event.date}
-          </p>
-          <p>{event.location}</p>
+          <div className="time">
+            <IoMdTime className="timeIcon" />
+            <p>
+              {weekdays[dayIndex]}, {event.date}
+            </p>
+          </div>
+          <div className="location">
+            <IoLocationOutline className="locationIcon" />
+            <p>{event.location}</p>
+          </div>
         </div>
-
+        {/* weather */}
+        <h4>Current weather</h4>
+        <Weather geo={geo} />
+        {/* map view */}
         {geo.length === 2 && (
           <MapView geo={geo} title={event.title} location={event.location} />
         )}
---
+++ frontend/src/pages/Events/EventDetail/Weather.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import useAxios from "../../../hooks/useAxios";
+import "./EventDetail.css";
+const Weather = ({ geo }) => {
+  const [weather, setWeather] = useState(null);
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${geo[0]}&lon=${geo[1]}&appid=${apiKey}`;
+  const { get, loading } = useAxios();
+
+  useEffect(() => {
+    if (geo.length === 2) {
+      const fetchWeather = async () => {
+        const weatherData = await get(apiUrl);
+        setWeather(weatherData);
+        console.log(weatherData);
+      };
+      fetchWeather();
+    }
+  }, [geo]);
+  if (loading) return <p>Loading weather forecast.....</p>;
+  return (
+    <div className="weatherContainer">
+      <div className="weatherInfo">
+        <p>{weather.weather[0].description}</p>
+        <p>{weather.main.temp}°C</p>
+        <p>💧 {weather.main.humidity}%</p>
+      </div>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+        alt="weatherIcon"
+        className="weatherIcon"
+      />
+    </div>
+  );
+};
+
+export default Weather;
