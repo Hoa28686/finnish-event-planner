d603639026c9a9a14f21c7aaa87884616cdb2297
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-06-02 15:27:29 +0300

    change small react-calendar to react-big-calendar

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -7,3 +7,15 @@ export const weekdays = [
   "friday",
   "saturday",
 ];
+
+export const categoryColors = {
+  food: "#EF4444",
+  tech: "#0EA5E9",
+  wellness: "#10B981",
+  market: "#F59E0B",
+  music: "#8B5CF6",
+  nature: "#166534",
+  art: "#EC4899",
+  fitness: "#6366F1",
+  games: "#F97316",
+};
--- frontend/src/pages/Calendar/Calendar.jsx
+++ frontend/src/pages/Calendar/Calendar.jsx
@@ -1,30 +1,29 @@
-import ReactCalendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import styles from "./Calendar.module.css";
-const Calendar = ({ events }) => {
-  const dayContent = ({ date, view }) => {
-    if (view === "month") {
-      const found = events.find((event) => {
-        const eventDate = new Date(event.date);
-        return (
-          eventDate.getFullYear() === date.getFullYear() &&
-          eventDate.getMonth() === date.getMonth() &&
-          eventDate.getDate() === date.getDate()
-        );
-      });
-      return found ? (
-        <div className={styles.foundEvents}>
-          <br />
-          <span className={styles.eventTitle}>{found.title}</span>
-        </div>
-      ) : null;
-    }
-  };
+import { categoryColors } from "../../data/data";
+const localizer = momentLocalizer(moment);
+
+const EventCalendar = ({ eventData }) => {
+  
   return (
-    <div className="container">
-      <ReactCalendar className={styles.calendar} tileContent={dayContent} />
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={eventData}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        className={styles.calendar}
+        eventPropGetter={(event) => ({
+          style: {
+            backgroundColor: categoryColors[event.category] || "#0EA5E9",
+          },
+        })}
+      />
     </div>
   );
 };
 
-export default Calendar;
+export default EventCalendar;
